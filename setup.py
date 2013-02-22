d4f59fc7e621ddb3351b03310ebb480a9140e15e
Damon Jablons <damonjablons@gmail.com>
2013-02-22 18:01:42 -0500

    Added setup for python package

---
+++ setup.py
@@ -0,0 +1,15 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='markdown-superscript',
+    version='0.1',
+    author='Shane Graber',
+    description='Python-Markdown extension which adds superscript',
+    url='https://github.com/blackrobot/markdown.superscript',
+    py_modules=['markdown_superscript'],
+    install_requires=['markdown>=2.0'],
+    zip_safe = True,
+    keywords='markdown superscript'
+)
