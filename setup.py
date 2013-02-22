934a5292f20c394d725bfba1806fcbb9b0af3b96
Damon Jablons <damonjablons@gmail.com>
2013-02-22 18:08:22 -0500

    Moved script to standard naming convention

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     author='Shane Graber',
     description='Python-Markdown extension which adds superscript',
     url='https://github.com/blackrobot/markdown.superscript',
-    py_modules=['markdown_superscript'],
+    py_modules=['mdx_superscript'],
     install_requires=['markdown>=2.0'],
     zip_safe = True,
     keywords='markdown superscript'
